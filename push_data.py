4c4b1592c6f87ef36ff26a4cbde1d7195ada1714
Andrew Piispanen <apiispanen1@babson.edu>
2022-10-05 00:24:53 -0400

    streamlit with processing

---
+++ push_data.py
@@ -0,0 +1,60 @@
+# PUSHES DATA INTO A GOOGLE SHEET
+
+from datetime import datetime as dt
+import numpy as np
+#### GET MY GOOGLE SHEET DATA
+import sys
+import subprocess
+import pandas as pd
+from pre_combined import *
+# combined_with_manual_sku_map
+
+scope =  ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive.file', "https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive"]
+
+import gspread
+from gspread_dataframe import set_with_dataframe
+from oauth2client.service_account import ServiceAccountCredentials
+
+# KEEP CREDS.JSON, AS IT SERVES THE AUTH KEY FOR THIS PROJECT
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("CANN SALES").worksheet("Sheet1")
+sheet.clear()
+set_with_dataframe(worksheet=sheet, dataframe=combined_with_manual_sku_map, include_index=False,
+include_column_header=True, resize=True)
+
+success_messsage = 'Import Completed!'
+
+
+# data = sheet.get_all_records()
+# print(data)
+
+#### UNCOMMENT ABOVE ^^^^ 
+
+# IF YOU JUST WANT SAMPLE DATA, UNOMMENT BELOW:
+# data = [{'Timestamp': '9/8/2022 0:33:02', 'How Do You Feel?': 4}, {'Timestamp': '9/8/2022 0:51:30', 'How Do You Feel?': 5}, {'Timestamp': '9/8/2022 10:04:27', 'How Do You Feel?': 3}, {'Timestamp': '9/9/2022 7:40:20', 'How Do You Feel?': 3}, {'Timestamp': '9/9/2022 11:45:42', 'How Do You Feel?': 3}, {'Timestamp': '9/9/2022 13:56:57', 'How Do You Feel?': 2}, {'Timestamp': '9/9/2022 19:03:37', 'How Do You Feel?': 4}, {'Timestamp': '9/9/2022 21:31:01', 'How Do You Feel?': 3}, {'Timestamp': '9/10/2022 11:44:07', 'How Do You Feel?': 3}]
+# END OF SAMPLE 
+
+
+# y_axis = [line['How Do You Feel?'] for line in data]
+# x_axis = [dt.strptime(line["Timestamp"], "%m/%d/%Y %H:%M:%S") for line in data]
+# data = [{'timestamp':x_axin, 'mood':y_axin} for y_axin, x_axin in zip(y_axis, x_axis)]  
+
+
+# workbooker = client.open("How do you feel")
+# sheet = workbooker.worksheet('Gym Records')
+# fitness_data = sheet.get_all_records()
+# dates_visited_gym = [dt.strptime(line['Date'], "%m/%d/%Y") for line in fitness_data]
+# data = pd.DataFrame(data)
+# daily_data = data.groupby(by=[pd.Grouper(key='timestamp', axis=0, freq='D')]).mean()
+# daily_data.index
+# gym_df = pd.DataFrame(dates_visited_gym, index=dates_visited_gym)
+# gym_df[0] = 1
+# gym_df.index = pd.to_datetime(gym_df.index)
+# gym_df.index = gym_df.index.rename('timestamp')
+# daily_data = daily_data.merge(gym_df, on='timestamp', how='left').fillna(0)
+# daily_data.columns = ['mood', 'gym_visit']
+
+# daily_data['timestamp-1'] = daily_data.index - pd.offsets.DateOffset(days=1)
+# daily_data['timestamp-2'] = daily_data.index - pd.offsets.DateOffset(days=2)
+# daily_data['timestamp-3'] = daily_data.index - pd.offsets.DateOffset(days=3)
--- stream.py
+++ stream.py
@@ -1,32 +1,84 @@
 import streamlit as st
+from io import BytesIO
+import subprocess
+import sys
 
-st.write('test')
+# CHECK DEPENDENCIES:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'gspread_dataframe'])
 
-options = st.multiselect(
-    'Databases',
+st.session_state['processed'] = 0
+
+from pyxlsb import open_workbook as open_xlsb
+
+def load_data():
+    st.session_state['processed'] = False
+    options = c.multiselect(
+    'View Raw Databases',
     ['CA', 'NV', 'IL', 'MA'])
+    with st.expander("Open to View Raw Databases"):
+        for option in options:
+            if option == "CA":
+                from pre_ca import ca_to_use
+                st.write(ca_to_use)
+            if option == "IL":
+                from pre_il import il_updated_with_date
+                st.write(il_updated_with_date)
+            if option == "NV":
+                from pre_nv import nv_updated_with_date
+                st.write(nv_updated_with_date)        
+            if option == "MA":
+                from pre_ma import ma_updated_with_date
+                st.write(ma_updated_with_date)
+
+
+c = st.container()
+c.write('SOCALI TABLEAU DATA ETL PREPROCESSOR')
+if c.button("Load Data"):
+    load_data()
+    st.session_state['load_data'] = True
+else:
+    load_data()
+
+
+def to_excel(df):
+    import pandas as pd
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def google_it():
+    from push_data import success_messsage
+    st.write(success_messsage)
+    st.balloons()
+
+
 
 def process():
-    from pre_combined import combined_with_manual_sku_map
-    st.download_button('Download', combined_with_manual_sku_map.to_csv().encode('utf-8'))
+    with st.spinner(text="Being Processed... View Prints in Console Log"):
+        from pre_combined import combined_with_manual_sku_map
+        st.session_state['processed'] = True
+        return combined_with_manual_sku_map
 
 
-for option in options:
-    if option == "CA":
-        from pre_ca import *
-        st.write(ca_to_use)
-    if option == "IL":
-        from pre_il import *
-        st.write(il_updated_with_date)
-    if option == "NV":
-        from pre_nv import *
-        st.write(nv_updated_with_date)        
-    if option == "MA":
-        from pre_ma import *
-        st.write(ma_updated_with_date)
-    
+if st.button("Process"):
+    combined_with_manual_sku_map = process()
 
 
+while st.session_state['processed'] == True:
+    col1, col2 = st.columns(2)
+    df_xlsx = to_excel(combined_with_manual_sku_map)
 
-st.write('You selected:', options)
-st.button("Process",on_click=process)
+    col1, col2 = st.columns(2)
+    col1.download_button('📥 Download', df_xlsx, file_name='CANN_SALES.xlsx')
+    col2.button('Push to Tableau', on_click=google_it)
+
+
+st.write(st.session_state)
